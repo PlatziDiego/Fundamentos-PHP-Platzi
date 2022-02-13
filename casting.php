562740e236907716108e7ad1215e328d4dbb6115
Diego Herrera <daherre0@gmail.com>
2022-02-13 15:46:48 -0500

    Casting

---
+++ casting.php
@@ -0,0 +1,25 @@
+<?php
+
+    $numerito = "5";
+
+    var_dump($numerito);
+
+    $numerito = (int) $numerito;
+    var_dump($numerito);
+
+    $numero = (int) 45;
+    $numero = "carlos";
+    var_dump($numero);
+
+
+    $dias = 5.89;
+    $dias = (int) $dias;
+    var_dump($dias);
+
+    $entero_no_cero = 4395870;
+    $entero_no_cero = (bool) $entero_no_cero;
+    var_dump($entero_no_cero);
+
+
+
+?>
