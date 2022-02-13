b4f5e0bbfd38f6ccbf35b006891c93fc38727991
Diego Herrera <daherre0@gmail.com>
2022-02-13 10:28:02 -0500

    Tipos

---
+++ tipos.php
@@ -0,0 +1,21 @@
+<?php
+    $numerito = "34750";
+    $numerito = $numerito + 45;
+    $nuevo_numerito = $numerito . "54";
+
+    var_dump($numerito);
+    var_dump($nuevo_numerito);
+    echo "\n";
+
+    $numerito_2 = 10;
+    $numerito_2 = $numerito_2 + 0.58009897987986;
+
+    var_dump($numerito_2);
+
+
+    $papas = "10 papas en el costal.";
+    $cuantas_papas_hay = $papas +5;
+    echo $cuantas_papas_hay;
+
+?>
+
